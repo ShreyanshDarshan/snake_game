e2e524483cec68f5eed6aae85adf793388750a13
ShreyanshDarshan <shreyanshdarshan1@gmail.com>
2021-03-30 09:25:07 +0530

    Snake Movement and Animation

---
+++ Assets/Scripts/Snake/SnakeMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1.0f;
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.velocity = Vector2.up * movementSpeed;
+    }
+}
--- Assets/Scripts/World/CameraSettings.cs
+++ Assets/Scripts/World/CameraSettings.cs
@@ -6,6 +6,7 @@ public class CameraSettings : MonoBehaviour
 {
     Camera cam;
     public TileGeneration tilegen;
+    [SerializeField] float extraSpacePercent = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class CameraSettings : MonoBehaviour
         // to help position the camera at the center of the grid
         Vector3 TileWidthOffset = new Vector3(tilegen.TileWidth, tilegen.TileWidth, 0);
         transform.position = new Vector3(tilegen.M / 2.0f, tilegen.N / 2.0f, -1) - TileWidthOffset/2;
+        cam.orthographicSize = (100 + extraSpacePercent) * tilegen.N / 200.0f;
         Debug.Log(cam.aspect);
     }
 
